e346d9906017b5947e281992d5284c2fc6da6bcc
Aayush jain <aayushjain1118@gmail.com>
2021-03-18 18:59:44 +0530

    home page drag and move

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,5 +1,5 @@
-import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useContext, useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import NotesContext from './NotesContext';
 import { Responsive, WidthProvider } from 'react-grid-layout';
 
@@ -16,6 +16,21 @@ function Grid() {
 		'#75c57d',
 		'#FF7CC3',
 	];
+	const history = useHistory();
+
+	const [allowForward, setAllowForward] = useState(true);
+	const [allowDrag, setAllowDrag] = useState(false);
+
+	const allowMove = () => {
+		setAllowForward(!allowForward);
+		setAllowDrag(!allowDrag);
+	};
+
+	const forwardTo = () => {
+		if (allowForward) {
+			history.push('/create');
+		}
+	};
 
 	return (
 		<>
@@ -39,17 +54,16 @@ function Grid() {
 				</Link>
 			</nav>
 
-			{/* <section className='grid'> */}
 			<ResponsiveGridLayout
 				className='layout'
-				cols={{ lg: 2, xxs: 2 }}
+				cols={{ lg: 2, md: 2, sm: 2, xs: 2, xxs: 2 }}
 				margin={[10, 10]}
 				isResizable={false}
 				compactType='vertical'
 				isBounded={true}
+				isDraggable={allowDrag}
 			>
 				{notes.map((x, i) => (
-					// <Link to={`/note/${x.id}`} key={x.id}>
 					<div
 						key={x.id}
 						data-grid={{
@@ -60,37 +74,57 @@ function Grid() {
 						}}
 					>
 						<div
-							className='card'
+							className={allowDrag ? 'draggable card' : 'card'}
 							style={{
 								background: colors[i % colors.length],
 							}}
+							onClick={() => forwardTo(x.id)}
 						>
 							<h4 className='card-title'>{x.title}</h4>
 							<p className='card-date'>{x.date}</p>
 						</div>
 					</div>
-					// </Link>
 				))}
 			</ResponsiveGridLayout>
 
-			{/* </section> */}
-
-			<Link to={'/create'} className='createBtn'>
-				<svg
-					xmlns='http://www.w3.org/2000/svg'
-					width='24'
-					height='24'
-					viewBox='0 0 24 24'
-					fill='none'
-					stroke='currentColor'
-					strokeWidth='1.7'
-					strokeLinecap='round'
-					strokeLinejoin='round'
-				>
-					<line x1='12' y1='5' x2='12' y2='19'></line>
-					<line x1='5' y1='12' x2='19' y2='12'></line>
-				</svg>
-			</Link>
+			<div className='floating'>
+				<button className='moveBtn' onClick={allowMove}>
+					<svg
+						xmlns='http://www.w3.org/2000/svg'
+						width='24'
+						height='24'
+						viewBox='0 0 24 24'
+						fill='none'
+						stroke='currentColor'
+						strokeWidth='1.7'
+						strokeLinecap='round'
+						strokeLinejoin='round'
+					>
+						<polyline points='5 9 2 12 5 15'></polyline>
+						<polyline points='9 5 12 2 15 5'></polyline>
+						<polyline points='15 19 12 22 9 19'></polyline>
+						<polyline points='19 9 22 12 19 15'></polyline>
+						<line x1='2' y1='12' x2='22' y2='12'></line>
+						<line x1='12' y1='2' x2='12' y2='22'></line>
+					</svg>
+				</button>
+				<Link to={'/create'} className='createBtn'>
+					<svg
+						xmlns='http://www.w3.org/2000/svg'
+						width='24'
+						height='24'
+						viewBox='0 0 24 24'
+						fill='none'
+						stroke='currentColor'
+						strokeWidth='1.7'
+						strokeLinecap='round'
+						strokeLinejoin='round'
+					>
+						<line x1='12' y1='5' x2='12' y2='19'></line>
+						<line x1='5' y1='12' x2='19' y2='12'></line>
+					</svg>
+				</Link>
+			</div>
 		</>
 	);
 }
